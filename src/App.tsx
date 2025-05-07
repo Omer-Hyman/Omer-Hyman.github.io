1401c9fa9a713413ccc028fb64a3db15b0c61c3a
Omer Hyman <omerhyman@gmail.com>
2025-05-08 00:52:20 +0100

    Styling and content changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import useEmblaCarousel from 'embla-carousel-react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import NavBarItem from './NavBarItem';
 
 function App() {
@@ -7,19 +7,13 @@ function App() {
 
   const [emblaRef, emblaApi] = useEmblaCarousel({
     loop: false,
-    dragFree: true,
+    dragFree: false,
     duration: 15,
+    watchDrag: false,
   });
 
   const [activeNavBarItem, setActiveNavBarItem] = useState(0);
   const [navBarItemInView, setNavBarItemInView] = useState(0);
-
-  useEffect(() => {
-    if (emblaApi) {
-      console.log(emblaApi.slideNodes());
-      console.log(emblaApi.selectedScrollSnap());
-    }
-  }, [emblaApi?.selectedScrollSnap()]);
 
   const navBarItemClicked = (index: number) => {
     emblaApi?.scrollTo(index);
@@ -28,12 +22,15 @@ function App() {
   };
 
   return (
-    <div className='h-full bg-gray-900 p-4 font-[roboto] text-white'>
+    <div className='h-full p-4 font-[roboto]'>
       <header className='mb-4 flex flex-col items-center'>
         <h1 className='mb-1.5 justify-center text-5xl'>Omer Hyman</h1>
         <h2 className='text-2xl'>Software Developer</h2>
 
-        <nav id='navBar' className='mt-4 flex w-[60%] justify-center'>
+        <nav
+          id='navBar'
+          className='mt-4 flex w-[90%] justify-center sm:w-[60%]'
+        >
           <ul className='align-center grid grid-cols-3 justify-center text-center text-lg'>
             <li
               onMouseEnter={() => {
@@ -90,30 +87,98 @@ function App() {
         </nav>
       </header>
 
-      <div className='embla m-auto max-w-[80%] overflow-hidden' ref={emblaRef}>
+      <div
+        className='embla m-auto max-w-[90%] overflow-hidden sm:max-w-[80%]'
+        ref={emblaRef}
+      >
         <div className='embla__container flex'>
           <div className='embla__slide min-w-0 flex-[0_0_100%]' id='aboutMe'>
             I am a software engineer with 1.5 years of industry experience, a
             degree in Computer Science and nearly 10 years of development
             experience. Although not officially working as a software engineer
-            since 2024, I have been always been working on personal projects in
-            my spare time. I love creating new products that I would love to
-            have and use in my own personal life. While I have used a wide
-            variety of different technologies, I always find myself coming back
-            to full stack cross platform web deveelopment. I love using React or
-            Angular alongside Capacitor to build web based applications that run
-            on Mobile, Desktop or the Web. It is really a powerful tool that
-            allows me to use my web development skills to write a cross platform
-            application.
-            {/* <br></br>
-            I have developed a lot during my development career, both
-            personally and technically. 
-            <br></br>
-            In my spare time I like skateboarding */}
+            since 2024, I have always been working on personal projects in my
+            spare time. I love creating new products that I wish existed for my
+            own personal use. While I have used a wide variety of different
+            technologies, I always find myself coming back to full stack cross
+            platform web development. More often than not, I am coding with
+            React or Angular alongside Capacitor to build web based applications
+            that run on Mobile, Desktop or the Web. Capacitor is really a
+            powerful tool that allows me to use my web development skills to
+            write a cross platform applications that have the chance to reach a
+            wider audience.
+            {/* List of skills e.g. ci/cd... */}
           </div>
           <div className='embla__slide min-w-0 flex-[0_0_100%]' id='projects'>
-            <h3>Event-based Spotify Playlist Generator</h3>
-            <h3>xSocial</h3>
+            <h3 className='mb-3'>
+              Spotify Playlist Generator based on Live Music Events
+            </h3>
+            <ul className='mb-3 flex flex-col gap-4'>
+              <li>
+                Built a responsive, cross-platform, full-stack responsive web
+                application integrating a ticketing platform and Spotify API to
+                allow users to generate custom Spotify playlists based on live
+                music events.
+              </li>
+              <li>
+                The app manages user authentication, data aggregation, and
+                seamless communication between multiple client devices and a
+                cloud-hosted server.
+              </li>
+              <li>
+                Developing this project has taught me the importance of some
+                tools used in industry to manage big complex applications. I
+                have been using a Kanban board, data flow diagrams to stay
+                organised, and have been carrying out testing ensure
+                maintainability.
+              </li>
+              <li>
+                Additionally, I have also been following SOLID principles
+                pragmatically to maintain a clean, loosely coupled and scalable
+                codebase.
+              </li>
+            </ul>
+
+            <iframe
+              className='m-auto'
+              src='https://www.youtube.com/embed/reI4LgNIHRM'
+              title=''
+              frameBorder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              referrerPolicy='strict-origin-when-cross-origin'
+              allowFullScreen
+            ></iframe>
+            {/* <video>
+              <source
+                src='https://www.youtube.com/watch?v=reI4LgNIHRM'
+                type='video/mp4'
+
+              />
+            </video> */}
+            <h3 className='mt-4 mb-3'>xSocial</h3>
+            <ul className='mb-3 flex flex-col gap-4'>
+              <li>
+                Developed a responsive frontend UI, set up a RESTful API and a
+                backend database system.
+              </li>
+              <li>
+                Connected to external APIs like Google Maps API and Leaflet to
+                provide a graphical map interface.
+              </li>
+              <li>
+                Used Ngrok to host the server allowing me to test across
+                multiple devices on different networks.
+              </li>
+            </ul>
+
+            <iframe
+              className='m-auto'
+              src='https://www.youtube.com/embed/gWqqvSPyAyM'
+              title='xSocial demo'
+              frameBorder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              referrerPolicy='strict-origin-when-cross-origin'
+              allowFullScreen
+            ></iframe>
           </div>
           <div className='embla__slide min-w-0 flex-[0_0_100%]' id='contactMe'>
             <h3>Links to all my socials</h3>
@@ -121,14 +186,7 @@ function App() {
         </div>
       </div>
 
-      {/* White border around bottom left and right, change to black at the bottom so white to black at 20% - maybe implement a shadow  */}
-
-      {/* Dark Mode by default - maybe extend to have a dark light mode switch */}
-      {/* Big Title - My name and maybe software developer */}
-      {/* Top bar - below title - spanning less than full width, each item has all except top border thin white - nice sliding animation to go into the next section  */}
-      {/* Sections in top bar: Intro?, Projects!, Tech stack/skills?, CV content (dont actually link CV, just the content), what I can do for you?, email and other links  */}
-
-      {/* TODO: Make sure you can't select the text in the nav buttons */}
+      {/* TODO: Add light mode/dark mode button */}
     </div>
   );
 }
--- src/NavBarItem.tsx
+++ src/NavBarItem.tsx
@@ -10,25 +10,25 @@ export default function NavBarItem({
   inView: boolean;
 }) {
   return (
-    <div className='relative cursor-default group h-[100%] flex justify-center'>
+    <div className='group relative flex h-[100%] cursor-default justify-center'>
       <span
-        className={`absolute inset-x-0 bottom-0 h-[2px] bg-gray-200 rounded-b-md group-hover:opacity-100 transition-opacity ${
+        className={`absolute inset-x-0 bottom-0 h-[2px] rounded-b-md bg-[#FF4500] transition-opacity group-hover:opacity-100 ${
           active ? 'opacity-100' : 'opacity-0'
         }`}
       ></span>
       <span
-        className={`absolute top-0 bottom-0 left-0 w-[2px] bg-gradient-to-t from-gray-200 to-transparent to-90% rounded-bl-md group-hover:opacity-100 transition-opacity ${
+        className={`absolute top-0 bottom-0 left-0 w-[2px] rounded-bl-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% transition-opacity group-hover:opacity-100 ${
           active ? 'opacity-100' : 'opacity-0'
         }`}
       ></span>
       <span
-        className={`absolute top-0 bottom-0 right-0 w-[2px] bg-gradient-to-t from-gray-200 to-transparent to-90% rounded-br-md group-hover:opacity-100 transition-opacity ${
+        className={`absolute top-0 right-0 bottom-0 w-[2px] rounded-br-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% transition-opacity group-hover:opacity-100 ${
           active ? 'opacity-100' : 'opacity-0'
         }`}
       ></span>
 
       <button
-        className={`pl-3 pr-3 pb-2 text-center ${
+        className={`pr-3 pb-2 pl-3 text-center ${
           inView ? 'font-semibold' : ''
         }`}
         onClick={() => clicked()}
