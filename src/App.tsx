5ccbeb83faba1af351c77aba016981566b8628f7
Omer Hyman <omerhyman@gmail.com>
2025-05-08 15:25:36 +0100

    Updated stylings and added social media links

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import useEmblaCarousel from 'embla-carousel-react';
 import { useState } from 'react';
 import NavBarItem from './NavBarItem';
+import { FaLinkedin } from 'react-icons/fa';
+import { VscGithub } from 'react-icons/vsc';
+import { FaSquareUpwork } from 'react-icons/fa6';
 
 function App() {
   // const [sectionInView, setSectionInView] = useState(0);
@@ -23,9 +26,33 @@ function App() {
 
   return (
     <div className='h-full p-4 font-[roboto]'>
-      <header className='mb-4 flex flex-col items-center'>
-        <h1 className='mb-1.5 justify-center text-5xl'>Omer Hyman</h1>
-        <h2 className='text-2xl'>Software Developer</h2>
+      <header className='mb-3 flex flex-col items-center'>
+        <h1 className='mb-1.5 justify-center'>Omer Hyman</h1>
+        <h2 className='mb-2'>Software Developer</h2>
+
+        <div id='iconBar' className='mt-1 flex gap-3'>
+          <FaLinkedin
+            className='text-4xl transition duration-125 hover:scale-130'
+            onClick={() => {
+              window.location.href =
+                'https://www.linkedin.com/in/omer-hyman-34287b151/';
+            }}
+          />
+
+          <VscGithub
+            className='text-4xl duration-125 hover:scale-130'
+            onClick={() => {
+              window.location.href = 'https://github.com/Omer-Hyman';
+            }}
+          />
+          <FaSquareUpwork
+            className='text-4xl duration-125 hover:scale-130'
+            onClick={() => {
+              window.location.href =
+                'https://www.upwork.com/freelancers/~0196bcb58c81e91a97';
+            }}
+          />
+        </div>
 
         <nav
           id='navBar'
@@ -33,53 +60,50 @@ function App() {
         >
           <ul className='align-center grid grid-cols-3 justify-center text-center text-lg'>
             <li
-              onMouseEnter={() => {
-                setActiveNavBarItem(0);
-              }}
-              onMouseLeave={() => {
-                setActiveNavBarItem(navBarItemInView);
-              }}
+            // onMouseEnter={() => {
+            //   setActiveNavBarItem(0);
+            // }}
+            // onMouseLeave={() => {
+            //   setActiveNavBarItem(navBarItemInView);
+            // }}
             >
               <NavBarItem
                 name='About Me'
                 clicked={() => {
                   navBarItemClicked(0);
                 }}
-                active={activeNavBarItem === 0}
                 inView={navBarItemInView === 0}
               />
             </li>
             <li
-              onMouseEnter={() => {
-                setActiveNavBarItem(1);
-              }}
-              onMouseLeave={() => {
-                setActiveNavBarItem(navBarItemInView);
-              }}
+            // onMouseEnter={() => {
+            //   setActiveNavBarItem(1);
+            // }}
+            // onMouseLeave={() => {
+            //   setActiveNavBarItem(navBarItemInView);
+            // }}
             >
               <NavBarItem
                 name='Projects'
                 clicked={() => {
                   navBarItemClicked(1);
                 }}
-                active={activeNavBarItem === 1}
                 inView={navBarItemInView === 1}
               />
             </li>
             <li
-              onMouseEnter={() => {
-                setActiveNavBarItem(2);
-              }}
-              onMouseLeave={() => {
-                setActiveNavBarItem(navBarItemInView);
-              }}
+            // onMouseEnter={() => {
+            //   setActiveNavBarItem(2);
+            // }}
+            // onMouseLeave={() => {
+            //   setActiveNavBarItem(navBarItemInView);
+            // }}
             >
               <NavBarItem
-                name='Contact Me'
+                name='Skills'
                 clicked={() => {
                   navBarItemClicked(2);
                 }}
-                active={activeNavBarItem === 2}
                 inView={navBarItemInView === 2}
               />
             </li>
@@ -88,100 +112,97 @@ function App() {
       </header>
 
       <div
-        className='embla m-auto max-w-[90%] overflow-hidden sm:max-w-[80%]'
+        className='embla m-auto max-w-[95%] overflow-hidden sm:max-w-[80%]'
         ref={emblaRef}
       >
         <div className='embla__container flex'>
           <div className='embla__slide min-w-0 flex-[0_0_100%]' id='aboutMe'>
-            I am a software engineer with 1.5 years of industry experience, a
-            degree in Computer Science and nearly 10 years of development
-            experience. Although not officially working as a software engineer
-            since 2024, I have always been working on personal projects in my
-            spare time. I love creating new products that I wish existed for my
-            own personal use. While I have used a wide variety of different
-            technologies, I always find myself coming back to full stack cross
-            platform web development. More often than not, I am coding with
-            React or Angular alongside Capacitor to build web based applications
-            that run on Mobile, Desktop or the Web. Capacitor is really a
-            powerful tool that allows me to use my web development skills to
-            write a cross platform applications that have the chance to reach a
-            wider audience.
+            <p>
+              I am a software engineer with 1.5 years of industry experience, a
+              degree in Computer Science and nearly 10 years of development
+              experience.
+            </p>
+            <p>
+              Although not officially working as a software engineer since 2024,
+              I have always been working on personal projects in my spare time.
+              I love creating new products that I wish existed for my own
+              personal use.
+            </p>
+            <p>
+              While I have used a wide variety of different technologies, I
+              always find myself coming back to full stack cross platform web
+              development. More often than not, I am coding with React or
+              Angular alongside Capacitor to build web based applications that
+              run on Mobile, Desktop or the Web. Capacitor is really a powerful
+              tool that allows me to use my web development skills to write a
+              cross platform applications that have the chance to reach a wider
+              audience.
+            </p>
             {/* List of skills e.g. ci/cd... */}
           </div>
-          <div className='embla__slide min-w-0 flex-[0_0_100%]' id='projects'>
-            <h3 className='mb-3'>
+          <div
+            className='embla__slide mb-8 min-w-0 flex-[0_0_100%]'
+            id='projects'
+          >
+            <h3 className='mb-2'>
               Spotify Playlist Generator based on Live Music Events
             </h3>
-            <ul className='mb-3 flex flex-col gap-4'>
-              <li>
+            <ul className='mb-4' id='SpotifyEventPlaylistGenerator'>
+              <p>
                 Built a responsive, cross-platform, full-stack responsive web
                 application integrating a ticketing platform and Spotify API to
                 allow users to generate custom Spotify playlists based on live
                 music events.
-              </li>
-              <li>
+              </p>
+              <p>
                 The app manages user authentication, data aggregation, and
                 seamless communication between multiple client devices and a
                 cloud-hosted server.
-              </li>
-              <li>
+              </p>
+              <p>
                 Developing this project has taught me the importance of some
                 tools used in industry to manage big complex applications. I
                 have been using a Kanban board, data flow diagrams to stay
                 organised, and have been carrying out testing ensure
                 maintainability.
-              </li>
-              <li>
+              </p>
+              <p>
                 Additionally, I have also been following SOLID principles
                 pragmatically to maintain a clean, loosely coupled and scalable
                 codebase.
-              </li>
+              </p>
             </ul>
 
             <iframe
               className='m-auto'
               src='https://www.youtube.com/embed/reI4LgNIHRM'
-              title=''
-              frameBorder='0'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-              referrerPolicy='strict-origin-when-cross-origin'
+              title='Spotify Playlist Generator demo'
               allowFullScreen
             ></iframe>
-            {/* <video>
-              <source
-                src='https://www.youtube.com/watch?v=reI4LgNIHRM'
-                type='video/mp4'
-
-              />
-            </video> */}
-            <h3 className='mt-4 mb-3'>xSocial</h3>
-            <ul className='mb-3 flex flex-col gap-4'>
-              <li>
+            <h3 className='mt-8 mb-2'>xSocial</h3>
+            <div className='mb-3'>
+              <p>
                 Developed a responsive frontend UI, set up a RESTful API and a
                 backend database system.
-              </li>
-              <li>
+              </p>
+              <p>
                 Connected to external APIs like Google Maps API and Leaflet to
                 provide a graphical map interface.
-              </li>
-              <li>
+              </p>
+              <p>
                 Used Ngrok to host the server allowing me to test across
                 multiple devices on different networks.
-              </li>
-            </ul>
-
+              </p>
+            </div>
             <iframe
               className='m-auto'
-              src='https://www.youtube.com/embed/gWqqvSPyAyM'
+              src='https://www.youtube.com/embed/gWqqvSPyAyM?color=white'
               title='xSocial demo'
-              frameBorder='0'
-              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-              referrerPolicy='strict-origin-when-cross-origin'
               allowFullScreen
             ></iframe>
           </div>
           <div className='embla__slide min-w-0 flex-[0_0_100%]' id='contactMe'>
-            <h3>Links to all my socials</h3>
+            <h3></h3>
           </div>
         </div>
       </div>
--- src/NavBarItem.tsx
+++ src/NavBarItem.tsx
@@ -1,40 +1,38 @@
 export default function NavBarItem({
   name,
   clicked,
-  active,
   inView,
 }: {
   name: string;
   clicked: () => void;
-  active: boolean;
   inView: boolean;
 }) {
   return (
     <div className='group relative flex h-[100%] cursor-default justify-center'>
       <span
-        className={`absolute inset-x-0 bottom-0 h-[2px] rounded-b-md bg-[#FF4500] transition-opacity group-hover:opacity-100 ${
-          active ? 'opacity-100' : 'opacity-0'
-        }`}
+        className={
+          'absolute inset-x-0 bottom-0 h-[2px] rounded-b-md bg-[#FF4500] opacity-0 transition-opacity group-hover:opacity-100'
+        }
       ></span>
       <span
-        className={`absolute top-0 bottom-0 left-0 w-[2px] rounded-bl-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% transition-opacity group-hover:opacity-100 ${
-          active ? 'opacity-100' : 'opacity-0'
-        }`}
+        className={
+          'absolute top-0 bottom-0 left-0 w-[2px] rounded-bl-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% opacity-0 transition-opacity group-hover:opacity-100'
+        }
       ></span>
       <span
-        className={`absolute top-0 right-0 bottom-0 w-[2px] rounded-br-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% transition-opacity group-hover:opacity-100 ${
-          active ? 'opacity-100' : 'opacity-0'
-        }`}
+        className={
+          'absolute top-0 right-0 bottom-0 w-[2px] rounded-br-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% opacity-0 transition-opacity group-hover:opacity-100'
+        }
       ></span>
 
       <button
-        className={`pr-3 pb-2 pl-3 text-center ${
-          inView ? 'font-semibold' : ''
-        }`}
+        className={`pr-3 pb-2 pl-3 text-center ${inView ? 'font-extrabold' : ''} group-hover:font-semibold`}
         onClick={() => clicked()}
       >
         {name}
       </button>
     </div>
+
+    // Red borders only when hovering - bold text when on the page
   );
 }
