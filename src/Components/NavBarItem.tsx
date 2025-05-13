963d09070ccbf99e90573ddb6e5034c38f50b403
Omer Hyman <omerhyman@gmail.com>
2025-05-13 20:34:09 +0100

    Got rid of nav bar styling on button press

--- src/Components/NavBarItem.tsx
+++ src/Components/NavBarItem.tsx
@@ -11,17 +11,17 @@ export default function NavBarItem({
     <div className='group relative flex h-[100%] cursor-default justify-center'>
       <span
         className={
-          'absolute inset-x-0 bottom-0 h-[2px] rounded-b-md bg-[#FF4500] opacity-0 transition-opacity group-hover:opacity-100 group-active:opacity-100'
+          'absolute inset-x-0 bottom-0 h-[2px] rounded-b-md bg-[#FF4500] opacity-0 transition-opacity group-hover:opacity-100'
         }
       ></span>
       <span
         className={
-          'absolute top-0 bottom-0 left-0 w-[2px] rounded-bl-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% opacity-0 transition-opacity group-hover:opacity-100 group-active:opacity-100'
+          'absolute top-0 bottom-0 left-0 w-[2px] rounded-bl-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% opacity-0 transition-opacity group-hover:opacity-100'
         }
       ></span>
       <span
         className={
-          'absolute top-0 right-0 bottom-0 w-[2px] rounded-br-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% opacity-0 transition-opacity group-hover:opacity-100 group-active:opacity-100'
+          'absolute top-0 right-0 bottom-0 w-[2px] rounded-br-md bg-gradient-to-t from-[#FF4500] to-transparent to-90% opacity-0 transition-opacity group-hover:opacity-100'
         }
       ></span>
 
